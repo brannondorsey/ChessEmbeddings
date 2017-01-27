ee04b3a940777146ce2ee7287f2d252e10a2039d
Brannon Dorsey <brannon@brannondorsey.com>
2017-01-27 09:41:54 -0600

    Cjhanges to pgn_to_words.py

--- pgn_to_words.py
+++ pgn_to_words.py
@@ -9,9 +9,14 @@ import codecs, pickle
 # takes a 2d array of moves from each game
 # and flattens to 1d
 def flatten_moves(moves_2d):
+	# flattened = []
+	# while (len(moves_2d) > 0):
+	# 	flattened = flattened + moves_2d.pop(0)
+	# return flattened
 	flattened = []
-	while (len(moves_2d) > 0):
-		flattened = flattened + moves_2d.pop(0)
+	for moves in moves_2d:
+		for move in moves:
+			flattened.append(move)
 	return flattened
 
 def load_parse_and_pickle_games(infile, outfile):
@@ -31,6 +36,19 @@ def pickel_all_unique_tokens(games, outfile):
 	print('{} unique moves (tokens)'.format(len(flattened)))
 	pickle.dump(flattened, open(outfile, 'wb'))
 	return flattened
+
+# returns train, test split
+def load_and_split(infile, split=0.70):
+	with open(infile, 'r') as f:
+		moves = f.read()
+		s = int(len(moves) * 0.70)
+		return moves[0:s], moves[s:]
+
+def save_train_test(train, test):
+	with open('data/train_moves.txt', 'w') as f:
+		f.write(''.join(train))
+	with open('data/test_moves.txt', 'w') as f:
+		f.write(''.join(test))
 
 def print_game(game):
 	print('annotator', game.annotator)
@@ -55,20 +73,26 @@ def print_game(game):
 ## parse and save unique tokens
 # tokens = pickel_all_unique_tokens(games, 'data/tokens.pickle')
 
-# load games
+## load games
 # with open('data/10_million_lines.pickle', 'rb') as f:
 # 	games = pickle.load(f)
 # print('loaded {} games'.format(len(games)))
 
-games = load_parse_and_pickle_games('data/10_million_lines.pgn', 'data/10_million_lines.pickle')
+# # # games = load_parse_and_pickle_games('data/10_million_lines.pgn', 'data/10_million_lines.pickle')
 
-## parse and save all moves
-moves = flatten_moves([g.moves for g in games])
-print('found {} moves'.format(len(moves)))
-text = ' '.join(moves)
-with open('data/moves.txt', 'w') as f:
-	f.write(text)
-	print('wrote {} chars to file'.format(len(text)))
+# # ## parse and save all moves
+# moves = flatten_moves([g.moves for g in games])
+# print('found {} moves'.format(len(moves)))
+# text = ' '.join(moves)
+# with open('data/{}_moves.txt'.format(len(moves)), 'w') as f:
+# 	f.write(text)
+# 	print('wrote {} chars to file'.format(len(text)))
+
+train, test = load_and_split('data/14444236_moves.txt', 0.70)
+print('train: {}, test: {}'.format(len(train), len(test)))
+
+save_train_test(train, test)
+
 
 # load unique tokens
 # with open('data/tokens.pickle', 'rb') as f:
