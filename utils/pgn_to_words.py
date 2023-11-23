6d9633660246e961ffb98270ce2e94d4ab6f4b1f
Brannon Dorsey <brannon@brannondorsey.com>
2023-11-23 16:42:21 -0500

    Format pgn_to_words.py
    * Simplify flatten_moves()
    * Fix load_and_split() parameter usage

--- utils/pgn_to_words.py
+++ utils/pgn_to_words.py
@@ -1,74 +1,74 @@
 # this is a utility script and should not be run
-# on its own w/out editing. 
+# on its own w/out editing.
 # Rather comment/uncomment code and run
 # bits and pieces as needed per the task at hand.
 
 import pgn
 import codecs, pickle
+from itertools import chain
+
 
 # takes a 2d array of moves from each game
 # and flattens to 1d
 def flatten_moves(moves_2d):
-	# flattened = []
-	# while (len(moves_2d) > 0):
-	# 	flattened = flattened + moves_2d.pop(0)
-	# return flattened
-	flattened = []
-	for moves in moves_2d:
-		for move in moves:
-			flattened.append(move)
-	return flattened
+    return list(chain.from_iterable(moves_2d))
+
 
 def load_parse_and_pickle_games(infile, outfile):
-	games = None
-	with codecs.open(infile, encoding='ascii', errors='replace') as f:
-		contents = f.read()
-		games = pgn.loads(contents)
+    games = None
+    with codecs.open(infile, encoding="ascii", errors="replace") as f:
+        contents = f.read()
+        games = pgn.loads(contents)
 
-	pickle.dump(games, open(outfile, 'wb'))
-	return games
+    pickle.dump(games, open(outfile, "wb"))
+    return games
+
 
 def pickel_all_unique_tokens(games, outfile):
-	moves = [g.moves for g in games]
-	flattened = flatten_moves(moves)
-	print('{} moves'.format(len(flattened)))
-	flattened = list(set(flattened))
-	print('{} unique moves (tokens)'.format(len(flattened)))
-	pickle.dump(flattened, open(outfile, 'wb'))
-	return flattened
+    moves = [g.moves for g in games]
+    flattened = flatten_moves(moves)
+    print("{} moves".format(len(flattened)))
+    flattened = list(set(flattened))
+    print("{} unique moves (tokens)".format(len(flattened)))
+    pickle.dump(flattened, open(outfile, "wb"))
+    return flattened
+
 
 # returns train, test split
-def load_and_split(infile, split=0.70):
-	with open(infile, 'r') as f:
-		moves = f.read()
-		s = int(len(moves) * 0.70)
-		return moves[0:s], moves[s:]
+def load_and_split(infile, split=0.90):
+    with open(infile, "r") as f:
+        moves = f.read()
+        s = int(len(moves) * split)
+        return moves[0:s], moves[s:]
+
 
 def save_train_test(train, test):
-	with open('data/train_moves.txt', 'w') as f:
-		f.write(''.join(train))
-	with open('data/test_moves.txt', 'w') as f:
-		f.write(''.join(test))
+    with open("data/train_moves.txt", "w") as f:
+        f.write("".join(train))
+    with open("data/test_moves.txt", "w") as f:
+        f.write("".join(test))
+
 
 def print_game(game):
-	print('annotator', game.annotator)
-	print('black', game.black) 
-	print('date', game.date)
-	print('eco', game.eco)
-	print('event', game.event)
-	print('eventdate', game.eventdate)
-	print('fen', game.fen)
-	print('mode', game.mode)
-	print('moves', game.moves)
-	print('plycount', game.plycount)
-	print('result', game.result)
-	print('round', game.round)
-	print('site', game.site)
-	print('termination', game.termination)
-	print('time', game.time)
-	print('timecontrol', game.timecontrol)
-	print('white', game.white)
-	print('whiteelo', game.whiteelo)
+    print("annotator", game.annotator)
+    print("black", game.black)
+    print("date", game.date)
+    print("eco", game.eco)
+    print("event", game.event)
+    print("eventdate", game.eventdate)
+    print("fen", game.fen)
+    print("mode", game.mode)
+    print("moves", game.moves)
+    print("plycount", game.plycount)
+    print("result", game.result)
+    print("round", game.round)
+    print("site", game.site)
+    print("termination", game.termination)
+    print("time", game.time)
+    print("timecontrol", game.timecontrol)
+    print("white", game.white)
+    print("whiteelo", game.whiteelo)
+
 
 ## parse and save unique tokens
 # tokens = pickel_all_unique_tokens(games, 'data/tokens.pickle')
@@ -88,8 +88,8 @@ def print_game(game):
 # 	f.write(text)
 # 	print('wrote {} chars to file'.format(len(text)))
 
-train, test = load_and_split('data/14444236_moves.txt', 0.70)
-print('train: {}, test: {}'.format(len(train), len(test)))
+train, test = load_and_split("data/14444236_moves.txt", 0.90)
+print("train: {}, test: {}".format(len(train), len(test)))
 
 save_train_test(train, test)
 
